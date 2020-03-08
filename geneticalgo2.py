e24e15611f9e670cde6c6c380821e0219f3e82a2
Md. Mozammel Haque <56899011+akash990@users.noreply.github.com>
2020-03-08 16:27:23 +0600

    Add files via upload

---
+++ geneticalgo2.py
@@ -0,0 +1,73 @@
+
+from random import random
+
+class BaseGene(object):
+    """
+    Handles functions shared by multiple types of genes (both node and connection),
+    including crossover and calling mutation methods.
+    """
+    def __init__(self, key):
+        self.key = key
+
+    def __str__(self):
+        attrib = ['key'] + [a.name for a in self._gene_attributes]
+        attrib = ['{0}={1}'.format(a, getattr(self, a)) for a in attrib]
+        return '{0}({1})'.format(self.__class__.__name__, ", ".join(attrib))
+
+
+    def __lt__(self, other):
+        assert isinstance(self.key,type(other.key)), "Cannot compare keys {0!r} and {1!r}".format(self.key,other.key)
+        return self.key < other.key
+
+
+    @classmethod
+    def parse_config(cls, config, param_dict):
+        pass
+
+
+def get_config_params(cls):
+        params = []
+        if not hasattr(cls, '_gene_attributes'):
+            setattr(cls, '_gene_attributes', getattr(cls, '__gene_attributes__'))
+            warnings.warn(
+                "Class '{!s}' {!r} needs '_gene_attributes' not '__gene_attributes__'".format(
+                    cls.__name__,cls),
+                DeprecationWarning)
+        for a in cls._gene_attributes:
+            params += a.get_config_params()
+        return params
+
+
+  def init_attributes(self, config):
+        for a in self._gene_attributes:
+            setattr(self, a.name, a.init_value(config))
+
+
+   def mutate(self, config):
+        for a in self._gene_attributes:
+            v = getattr(self, a.name)
+            setattr(self, a.name, a.mutate_value(v, config))
+
+
+   def copy(self):
+        new_gene = self.__class__(self.key)
+        for a in self._gene_attributes:
+            setattr(new_gene, a.name, getattr(self, a.name))
+
+        return new_gene
+
+
+    def crossover(self, gene2):
+        """ Creates a new gene randomly inheriting attributes from its parents."""
+        assert self.key == gene2.key
+
+   
+        new_gene = self.__class__(self.key)
+        for a in self._gene_attributes:
+            if random() > 0.5:
+                setattr(new_gene, a.name, getattr(self, a.name))
+            else:
+                setattr(new_gene, a.name, getattr(gene2, a.name))
+
+        return new_genes
+		
